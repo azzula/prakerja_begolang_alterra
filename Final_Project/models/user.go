c7628ce17c33482f9b606fda821d2d20f50b2644
Muhammad Sufyan Ats Tsaurie <83033224+azzula@users.noreply.github.com>
2023-05-13 01:09:59 +0700

    update file and folder

--- Final_Project/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	Id			int			`json:"id"`
-	Name		string		`json:"name"`
-	Address		string		`json:"address"`
-	Email		string		`json:"email" gorm:"unique"`
-	Password	string		`json:"password"`
-}
